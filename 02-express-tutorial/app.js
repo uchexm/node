4525cfffa8166ff942437171e827db8dc6bd74ce
uchexm <109084069+uchexm@users.noreply.github.com>
2023-10-29 12:43:52 +0200

    event emitter

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello</h1>")
+  res.write("<h1>Hello this is Home page</h1>")
   res.end();
 });
 
