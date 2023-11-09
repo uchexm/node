c4413b0e59425271c80f181719f359b6967514b0
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-09 14:21:06 +0200

    event emitter

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,7 +1,10 @@
 const { createReadStream } = require("fs");
 
-const stream = createReadStream("./content/big.txt")
+const stream = createReadStream("./content/big.txt", { highWaterMark: 90000, encoding: 'utf8' });
 
 stream.on("data", (result) => {
   console.log(result);
+})
+stream.on("error", (err) => {
+  console.log(err);
 })
