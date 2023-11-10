ef98015388fdcf467518039f8654ef2b19abe5a9
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-10 14:17:09 +0200

    event emitter

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,4 +1,7 @@
-const { writeFileSync } = require("fs")
-for (let i = 0; i < 1000; i++) {
-  writeFileSync("./content/big.txt", `hello world ${i}\n`, { flag: "a" })
-}
+const { createReadStream } = require("fs");
+
+const stream = createReadStream("./content/big.txt")
+
+stream.on("data", (result) => {
+  console.log(result);
+})
