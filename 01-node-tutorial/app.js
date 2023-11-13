661b8db081b89112f607bab66addc1b291a9d419
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-13 11:00:42 +0200

    Async

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,7 +1,21 @@
-const { readFile } = require("fs");
-const { result } = require("lodash");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path) => {
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8")
+    const second = await readFilePromise("./content/second.txt", "utf8")
+    await writeFilePromise("./content/sec.txt", `This is awesome!! ${first} ${second}`)
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
+
+/*const getText = (path) => {
   return new Promise((resolve, reject) => {
     readFile(path, "utf8", (err, data) => {
       if (err) {
@@ -12,17 +26,9 @@ const getText = (path) => {
     })
   }
   )
-}
+}*/
+
+
 //getText("./content/first.txt").then(result => console.log(result)).catch(err => console.log(err));
 
 
-const start = async () => {
-  try {
-    const first = await getText("./content/first.txt")
-    const second = await getText("./content/second.txt")
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-}
-start();
