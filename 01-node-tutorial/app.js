23314542637693be8014188d447964f6a2168fc2
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-21 10:23:54 +0200

    server

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,7 +1,14 @@
+const { error } = require('console');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+  if (req.url === "/") {
+    res.end("Home page")
+  }
+  if (req.url === "/about") {
+    res.end("About page")
+  }
+  res.end("Error page")
 
 }
 )
