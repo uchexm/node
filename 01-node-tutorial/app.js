d70ecd0e462aacd6121b1d2d2878341cf17b4217
uchexm <109084069+uchexm@users.noreply.github.com>
2023-11-27 09:52:06 +0200

    set timeout

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,10 +1,7 @@
-const { error } = require("console");
-const { readFile, writeFile } = require("fs");
+const _ = require("lodash");
 
-readFile("./content/first.txt", "utf8", (error, result) => {
-  if (error) {
-    console.log(error);
-    return
-  }
-  console.log(result);
-})
+const items = [1, [2, 3, [4]]];
+const newItems = _.flattenDeep(items)
+console.log(newItems);
+
+console.log("Hello world!");
